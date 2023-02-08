53e5ae08a47b7ba53105635923bce9029e4c1c6c
cridder <cjridder@gmail.com>
2023-02-08 12:55:27 -0800

    20230208125524

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,39 +1,39 @@
 const logoutBtn = document.querySelector(".logout");
 const loginBtn = document.querySelector(".login");
 const signupBtn = document.querySelector(".signup");
-const createpostBtn = document.querySelector(".createpost");
+const createpostBtn = document.querySelector(".tbcreatepost");
 
 const logout = async (event) => {
-    event.preventDefault();
-    const response = await fetch("/api/users/logout", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    });  
-    if (response.ok) document.location.replace("/");
-  };
+	event.preventDefault();
+	const response = await fetch("/api/users/logout", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+	});
+	if (response.ok) document.location.replace("/");
+};
 
-  const login = async (event) => {
-    event.preventDefault();
-    document.location.replace("/login")
-  };
+const login = async (event) => {
+	event.preventDefault();
+	document.location.replace("/login");
+};
 
-  const createpost = async (event) => {
-    event.preventDefault();
-    document.location.replace("/api/posts")
-  };
-  
-  if (logoutBtn) {
-    logoutBtn.addEventListener("click", logout);
-  }
+const tbcreatepost = async (event) => {
+	event.preventDefault();
+	document.location.replace("/api/posts");
+};
 
-  if (loginBtn) {
-    loginBtn.addEventListener("click", login);
-  }
+if (logoutBtn) {
+	logoutBtn.addEventListener("click", logout);
+}
 
-  if (signupBtn) {
-    signupBtn.addEventListener("click", login);
-  }
+if (loginBtn) {
+	loginBtn.addEventListener("click", login);
+}
 
-  if (createpostBtn) {
-    createpostBtn.addEventListener("click", createpost);
-  }
+if (signupBtn) {
+	signupBtn.addEventListener("click", login);
+}
+
+if (createpostBtn) {
+	createpostBtn.addEventListener("click", tbcreatepost);
+}
